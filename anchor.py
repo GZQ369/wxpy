631513f26d45c26759d84a06489d919c8a7bba9f
gongzhiqiang <gongzhiqiang@360.cn>
2021-11-02 16:02:40 +0800

    up

--- anchor.py
+++ anchor.py
@@ -1,70 +1,76 @@
 from tkinter import *
 from tkinter import messagebox
 
-def closeallwindow():
-    window.destroy(  )
 
-def closeWindow():
-    messagebox.showinfo(title="警告",message="关不掉吧，气不气")
+def closeallroot():
+    root.destroy()
+
+
+def closeroot():
+    messagebox.showinfo(title="警告", message="关不掉吧，气不气")
     return
 
-def love():
-    love=Toplevel(window)
+
+def love(root):
+    love = Toplevel(root)
     love.geometry("300x150+610+260")
-    love.title( "好巧啊，我也喜欢你")
-    label=Label(love,text="如家酒店A350等你",font=("华文行楷",25))
+    love.title("好巧啊，我也喜欢你")
+    label = Label(love, text="如家酒店A350等你", font=("华文行楷", 25))
     label.pack()
-    label=Label(love,text="电话给我，美滋滋",font=("华文行楷",25))
+    label = Label(love, text="电话给我，美滋滋", font=("华文行楷", 25))
     label.pack()
 
-    entry=Entry(love,font=("楷体",15))
+    entry = Entry(love, font=("楷体", 15))
     entry.pack()
 
-    btn=Button(love,text="嗯嗯",width=10,height=2,command=closeallwindow)
+    btn = Button(love, text="嗯嗯", width=10, height=2, command=closeallroot)
     btn.pack()
-    love.protocol("WM_DELETE_WINDOW",closelove)
+    love.protocol("WM_DELETE_root", closelove)
+
 
 def closelove():
-    messagebox.showinfo(title="好怂啊你",message="喜欢我直说就行")
+    messagebox.showinfo(title="好怂啊你", message="喜欢我直说就行")
     return
 
-def noLove():
-    no_love=Toplevel(window)
+
+def noLove(root):
+    no_love = Toplevel(root)
     no_love.geometry("300x100+610+260")
-    no_love.title( "我好喜欢你")
-    label=Label(no_love,text="再考虑考虑呗",font=("华文行楷",25))
+    no_love.title("我好喜欢你")
+    label = Label(no_love, text="再考虑考虑呗", font=("华文行楷", 25))
     label.pack()
-    btn=Button(no_love,text="好吧",width=10,height=2,command=no_love.destroy)
+    btn = Button(no_love, text="好吧", width=10,
+                 height=2, command=no_love.destroy)
     btn.pack()
-    no_love.protocol("WM_DELETE_WINDOW",closeNoLove)
-
-def closeNoLove():
-    #messagebox.showinfo("不喜欢我，你就关不掉")
-    messagebox.showinfo(title="警告",message="不喜欢我，你就关不掉")
-    noLove()
+    no_love.protocol("WM_DELETE_root", closeNoLove)
 
 
-window=Tk()
-window.title("喜欢我么,来自一个漂亮女生的告白？")
-window.geometry("420x420+590+230")
-window.protocol("WM_DELETE_WINDOW",closeWindow)
-
-label1=Label(window,text="车佶，傻逼？气不气",
-             font=("华文行楷",16),fg="red")
-label1.grid()
-label2=Label(window,text="喜欢我么？",font=("华文行楷",30))
-label2.grid(row=1,column=1,sticky=E)
-
-photo=PhotoImage(file="./12345678/2.png")
-imageLable=Label(window,image=photo)
-imageLable.grid(row=2,columnspan=2)
-
-btn1=Button(window,text="愿意",width=15,height=2,command=love)
-btn1.grid(row=3,column=0,sticky=W)
-
-btn2=Button(window,text="不愿意",width=15,height=2,command=noLove)
-btn2.grid(row=3,column=1,sticky=E)
+def closeNoLove(root):
+    messagebox.showinfo("不喜欢我，你就关不掉")
+    messagebox.showinfo(title="警告", message="不喜欢我，你就关不掉")
+    noLove(root)
 
 
-window.mainloop()
+# root = Tk()
+# root.title("喜欢我么,来自一个漂亮女生的告白？")
+# root.geometry("420x420+590+230")
+# root.protocol("WM_DELETE_root", closeroot)
 
+# label1 = Label(root, text="车佶，傻逼？气不气",
+#                font=("华文行楷", 16), fg="red")
+# label1.grid()
+# label2 = Label(root, text="喜欢我么？", font=("华文行楷", 30))
+# label2.grid(row=1, column=1, sticky=E)
+
+# # photo=PhotoImage(file="./12345678/2.png")
+# # imageLable=Label(root,image=photo)
+# # imageLable.grid(row=2,columnspan=2)
+
+# btn1 = Button(root, text="愿意", width=15, height=2, command=love)
+# btn1.grid(row=3, column=0, sticky=W)
+
+# btn2 = Button(root, text="不愿意", width=15, height=2, command=noLove)
+# btn2.grid(row=3, column=1, sticky=E)
+
+
+# root.mainloop()
---
+++ tk.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import time, sys
+
+from tkinter import *
+from itchatwx import oneDayMsg
+from anchor import *
+mydelaymin = 3600 #窗口提示的延迟时间，以分钟计
+
+#------------------def-------------------
+def showMessage():
+	#show reminder message window
+	root = Tk()  #建立根窗口
+	#root.minsize(500, 200)   #定义窗口的大小
+	#root.maxsize(1000, 400)  #不过定义窗口这个功能我没有使用
+	root.withdraw()  #hide window
+	#获取屏幕的宽度和高度，并且在高度上考虑到底部的任务栏，为了是弹出的窗口在屏幕中间
+	screenwidth = root.winfo_screenwidth()
+	screenheight = root.winfo_screenheight() - 100 
+	root.resizable(False,False)
+	#添加组件
+	root.title("喜欢我么？")
+	frame = Frame(root, relief=RIDGE, borderwidth=3)
+	frame.pack(fill=BOTH, expand=1) #pack() 放置组件若没有则组件不会显示
+	#窗口显示的文字、并设置字体、字号
+	label = Label(frame, text=oneDayMsg(), \
+		font="Monotype\ Corsiva -20 bold")
+	label.pack(fill=BOTH, expand=1)
+	#按钮的设置
+	button = Button(frame, text="嗯嗯", font="Cooper -25 bold", fg="black", command=root.destroy)
+	button.pack(side=BOTTOM)
+
+
+	root.update_idletasks()
+	root.deiconify() #now the window size was calculated
+	root.withdraw() #hide the window again 防止窗口出现被拖动的感觉 具体原理未知？
+	root.geometry("{}x{}+{}+{}".format(root.winfo_width() + 10, root.winfo_height() + 10, 
+	int((screenwidth - root.winfo_width())/2), int((screenheight - root.winfo_height())/2)))
+	root.deiconify()
+	root.protocol("WM_DELETE_WINDOW",closeroot)
+
+	root.mainloop()
+ 
+# showMessage()
+ 
+ 
+while True:
+	showMessage()
+	time.sleep(mydelaymin*12) #参数为小时
